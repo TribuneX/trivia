184b7a28d581ea46048834b9d18c8268e5577945
TribuneX <saschableidner@gmail.com>
2017-11-17 13:34:56 +0100

    Refactor output of Questions in Game classπ

--- java/src/main/java/com/adaptionsoft/games/Question/QuestionInterface.java
+++ java/src/main/java/com/adaptionsoft/games/Question/QuestionInterface.java
@@ -2,6 +2,6 @@ package com.adaptionsoft.games.Question;
 
 public interface QuestionInterface {
 
-    public String getCategory();
-    public String getQuestion();
+    String getCategory();
+    String getQuestion();
 }
