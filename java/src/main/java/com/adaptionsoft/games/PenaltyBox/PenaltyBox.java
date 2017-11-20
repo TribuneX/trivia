b111c6757c084c46aad85ab27f0a9a48ea3e6c86
TribuneX <saschableidner@gmail.com>
2017-11-20 14:16:06 +0100

    Remove PenaltyBox Interface

--- java/src/main/java/com/adaptionsoft/games/PenaltyBox/PenaltyBox.java
+++ java/src/main/java/com/adaptionsoft/games/PenaltyBox/PenaltyBox.java
@@ -3,7 +3,7 @@ package com.adaptionsoft.games.PenaltyBox;
 import java.util.HashSet;
 import java.util.Set;
 
-public class PenaltyBox implements PenaltyBoxInterface {
+public class PenaltyBox {
 
 
     Set<String> box;
@@ -12,18 +12,12 @@ public class PenaltyBox implements PenaltyBoxInterface {
         box = new HashSet<>();
     }
 
-    @Override
     public void sendPlayerToTheBox(String name) {
         box.add(name);
     }
 
-    @Override
     public boolean isPlayerInTheBox(String name) {
         return box.contains(name);
     }
 
-    @Override
-    public void removePlayerFromTheBox(String name){
-        box.remove(name);
-    }
 }
